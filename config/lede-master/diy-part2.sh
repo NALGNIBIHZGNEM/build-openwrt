524894ec3919937351e1b43219cbf8282c6c59fd
NALGNIBIHZGNEM <772097880@qq.com>
2024-07-26 11:35:42 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -20,6 +20,19 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
 # sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+# 修改默认IP地址
+sed -i 's/192.168.1.1/192.168.66.253/g' package/base-files/files/bin/config_generate
+
+# 修改主机名
+sed -i "s/hostname='OpenWrt'/hostname='GBC'/g" package/base-files/files/bin/config_generate
+# 修改设备说明
+sed -i '/openwrt_release/d' package/lean/default-settings/files/zzz-default-settings
+sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='GBC $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
+# 写入设备型号
+echo "DISTRIB_MODEL='X86_64'" >> package/base-files/files/etc/openwrt_release
+# 调整 x86 型号只显示 CPU 型号
+sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}/g' package/lean/autocore/files/x86/autocore
+
 
 # Replace the default software source
 # sed -i 's#openwrt.proxy.ustclug.org#mirrors.bfsu.edu.cn\\/openwrt#' package/lean/default-settings/files/zzz-default-settings
